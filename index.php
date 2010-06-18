a6953f94a16f320d63199f931f81b1912792a483
Phil Tobias <philtobias@gmail.com>
2010-06-18 00:23:54 -0700

    Init commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Yosemite Sam</title>
+	<link rel="stylesheet" href="resources/css/style.css" type="text/css" />
+</head>
+<body>
+	<div id="dropbox">
+		Drop images here		
+	</div>
+	<div id="preview"></div>
+	
+	<script src="http://code.jquery.com/jquery.min.js"></script>	
+	<script src="resources/js/file-drag.js"></script>
+</body>
+</html>
