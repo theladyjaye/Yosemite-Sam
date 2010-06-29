3636de4f264a2044dcde6a7cb31ff7c9b2ca7cbe
Phil Tobias <philtobias@gmail.com>
2010-06-29 01:05:08 -0700

    Added views page

--- application/templates/body.php
+++ application/templates/body.php
@@ -1,120 +1,61 @@
 <div id="global-body">
 	<div id="view-table-header" class="view-header">
+		<?if($is_views):?>
+		<div class="page-title icon-view font-replace">			
+			Views <span class="count">(7)</span> 
+		<?else:?>
 		<div class="page-title icon-folder font-replace">
 			Projects <span class="count">(5)</span> 
+		<?endif;?>
 		</div>
+		<?if($is_views):?>
+		<a href="#" title="Add View" class="font-replace add icon-add no_">Add View</a>
+		<?else:?>
 		<a href="#" title="Add Project" class="font-replace add icon-add no_">Add Project</a>
+		<?endif;?>
 	</div>
 	<div id="view-editor-header" class="view-header">
 	</div>
 	
 	<div id="view-body">
 		<ul id="view-table-list">
-			<li>
-				<a href="#" title="" class="item-name font-replace">Apple</a>
+			<?php foreach($data as $i => $item): ?>			
+			<li class="<?if($i % 2):?>alt<?endif;?>">
+				<a href="<?if($is_views):?>#<?else:?>views.php<?endif;?>" title="" class="item-name font-replace"><?=$item["item-name"]?></a>
 				<a href="#" class="item-details font-replace">details</a>
 				<div class="progress-bar">
 					<div class="progress-value font-replace">
-						<span class="value">85</span>
+						<span class="value"><?=$item["percentage"]?></span>
 						<span class="percentage-sign">%</span>
 					</div>
 				</div>
 				<a href="#" class="delete icon-delete no_ font-replace">Delete</a>
 				
 				<div class="item-detail">
-					<p class="item-tasks icon-check">Tasks <span class="tasks-closed">84</span>/<span class="tasks-total">152</span></p>
-					<p class="item-views icon-view">Views <span class="view-count">7</span></p>
+					<p class="item-tasks icon-check">Tasks <span class="tasks-closed"><?=$item["tasks-closed"]?></span>/<span class="tasks-total"><?=$item["tasks-total"]?></span></p>
+					<?if($is_views):?>
+					<p class="item-states icon-states">States <span class="view-count"><?=count($item["states"])?></span>
+						(<?foreach($item["states"] as $state_dex => $state):?>
+							<a href="#"><?=$state?></a><?if($state_dex < count($item["states"]) - 1):?>,<?endif;?>
+						<?endforeach;?>)						
+					</p>
+					<p class="item-attachments">Attachments <span class="attachment-count"><?=count($item["attachments"])?></span>
+						<select class="dd-attachments">
+							<?foreach($item["attachments"] as $attachment_dex => $attachment):?>
+								<option value="<?=$attachment_dex?>"><?=$attachment?></option>
+							<?endforeach;?>
+						</select>
+						<a href="#" class="add-attachment icon-add-small no_">Add Attachment</a>
+					</p>
+					<?else:?>
+					<p class="item-views icon-view-small">Views <span class="view-count"><?=$item["views-count"]?></span></p>
+					<?endif;?>
 					<div class="item-desc">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
-
-						<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>
+						<?=$item["desc"]?>
 					</div>
 				</div>
 			</li>
-			<li class="odd">
-				<a href="#" title="" class="item-name font-replace">Gaikai</a>
-				<a href="#" class="item-details font-replace">details</a>
-				<div class="progress-bar">
-					<div class="progress-value font-replace">
-						<span class="value">35</span>
-						<span class="percentage-sign">%</span>
-					</div>
-				</div>
-				<a href="#" class="delete icon-delete no_ font-replace">Delete</a>
-				
-				<div class="item-detail">
-					<p class="item-tasks icon-check">Tasks <span class="tasks-closed">84</span>/<span class="tasks-total">152</span></p>
-					<p class="item-views icon-view">Views <span class="view-count">7</span></p>
-					<div class="item-desc">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
-
-						<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>
-					</div>
-				</div>
-			</li>
-			<li class="">
-				<a href="#" title="" class="item-name font-replace">Guitar Hero</a>
-				<a href="#" class="item-details font-replace">details</a>
-				<div class="progress-bar">
-					<div class="progress-value font-replace">
-						<span class="value">55</span>
-						<span class="percentage-sign">%</span>
-					</div>
-				</div>
-				<a href="#" class="delete icon-delete no_ font-replace">Delete</a>
-				
-				<div class="item-detail">
-					<p class="item-tasks icon-check">Tasks <span class="tasks-closed">84</span>/<span class="tasks-total">152</span></p>
-					<p class="item-views icon-view">Views <span class="view-count">7</span></p>
-					<div class="item-desc">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
-
-						<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>
-					</div>
-				</div>				
-			</li>
-			<li class="odd">
-				<a href="#" title="" class="item-name font-replace">Microsoft</a>
-				<a href="#" class="item-details font-replace">details</a>
-				<div class="progress-bar">
-					<div class="progress-value font-replace">
-						<span class="value">14</span>
-						<span class="percentage-sign">%</span>
-					</div>
-				</div>
-				<a href="#" class="delete icon-delete no_ font-replace">Delete</a>
-				
-				<div class="item-detail">
-					<p class="item-tasks icon-check">Tasks <span class="tasks-closed">84</span>/<span class="tasks-total">152</span></p>
-					<p class="item-views icon-view">Views <span class="view-count">7</span></p>
-					<div class="item-desc">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
-
-						<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>
-					</div>
-				</div>				
-			</li>
-			<li class="">
-				<a href="#" title="" class="item-name font-replace">Wunderman</a>
-				<a href="#" class="item-details font-replace">details</a>
-				<div class="progress-bar">
-					<div class="progress-value font-replace">
-						<span class="value">98</span> <!-- todo: fix 100% for progress bar -->
-						<span class="percentage-sign">%</span>
-					</div>
-				</div>
-				<a href="#" class="delete icon-delete no_ font-replace">Delete</a>
-				
-				<div class="item-detail">
-					<p class="item-tasks icon-check">Tasks <span class="tasks-closed">84</span>/<span class="tasks-total">152</span></p>
-					<p class="item-views icon-view">Views <span class="view-count">7</span></p>
-					<div class="item-desc">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
-
-						<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>
-					</div>
-				</div>				
-			</li>
+			<?endforeach;?>			
 		</ul>
 	</div>
 </div>
--- views.php
+++ views.php
@@ -5,55 +5,128 @@
 </head>
 <body>
 	<div id="container">
-		<?php include("application/templates/header.php"); ?>
-		<?php 
+		<?php
+			$is_views = true;
+		 	include("application/templates/header.php"); 
+		?>
+		<?php 			
 			$data = array(
 				array(
-					"item-name"=> "Apple",
-					"percentage"=> "55",
+					"item-name"=> "Buzz",
+					"percentage"=> "30",
 					"tasks-closed"=> 84,
 					"tasks-total"=> 152,
-					"views-count"=> 7,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
 					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
 
 					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
 				),
 				array(
-					"item-name"=> "Gaiaki",
-					"percentage"=> "13",
+					"item-name"=> "Games",
+					"percentage"=> "32",
 					"tasks-closed"=> 84,
 					"tasks-total"=> 152,
-					"views-count"=> 7,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
 					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
 
 					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
 				),
 				array(
-					"item-name"=> "Guitar Hero",
-					"percentage"=> "60",
+					"item-name"=> "Home",
+					"percentage"=> "49",
 					"tasks-closed"=> 84,
 					"tasks-total"=> 152,
-					"views-count"=> 7,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
 					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
 
 					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
 				),
 				array(
-					"item-name"=> "Microsoft",
+					"item-name"=> "Learn",
+					"percentage"=> "67",
+					"tasks-closed"=> 84,
+					"tasks-total"=> 152,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
+					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
+
+					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
+				),
+				array(
+					"item-name"=> "Media",
+					"percentage"=> "95",
+					"tasks-closed"=> 84,
+					"tasks-total"=> 152,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
+					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
+
+					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
+				),
+				array(
+					"item-name"=> "Music",
+					"percentage"=> "16",
+					"tasks-closed"=> 84,
+					"tasks-total"=> 152,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
+					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
+
+					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
+				),
+				array(
+					"item-name"=> "The Stage",
 					"percentage"=> "25",
 					"tasks-closed"=> 84,
 					"tasks-total"=> 152,
-					"views-count"=> 7,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
 					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
 
 					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
 				),
 				array(
-					"item-name"=> "Wunderman",
-					"percentage"=> "5",
+					"item-name"=> "Buzz",
+					"percentage"=> "55",
 					"tasks-closed"=> 84,
 					"tasks-total"=> 152,
-					"views-count"=> 7,
+					"states" => array(
+						"Logged In", "Logged Out"
+					),
+					"attachments" => array(
+						"wireframe", "tech spec", "func spec", "documentation"
+					),
 					"desc"=> "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis lorem ac nunc pretium ut sagittis mi egestas. Nullam ornare iaculis aliquam. Sed et neque dolor. Fusce rhoncus ligula ut felis venenatis pretium. Quisque enim lorem, sodales non mollis sit amet, condimentum a tellus. Curabitur eget augue lectus, et fermentum nisl. Nulla facilisi. Nulla pharetra dui sed libero ornare eget varius nibh sollicitudin. Vestibulum tincidunt pulvinar sodales. Etiam ac orci ut est dictum pharetra. Nam et diam purus. Phasellus ut libero vitae nulla aliquam pellentesque et facilisis enim. Sed sed nunc diam, sed venenatis nunc. Quisque rhoncus pharetra tempor. Suspendisse et elit dolor, ac placerat ipsum. Nulla purus mauris, consectetur nec mattis vitae, vulputate sit amet leo. Maecenas semper enim in tellus pretium vehicula.</p>
 
 					<p>Duis fermentum nulla at justo ultricies eu lobortis ipsum fringilla. Aenean adipiscing neque non nisl faucibus eget tincidunt felis tempor. Fusce rutrum leo sit amet mi dignissim a dictum libero dignissim. Mauris porta metus et nulla varius tempor. Pellentesque libero urna, sodales accumsan blandit sed, accumsan sed augue. Donec</p>"
